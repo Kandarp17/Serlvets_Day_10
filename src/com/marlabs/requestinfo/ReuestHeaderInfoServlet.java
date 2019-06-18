c0bcaeba525dffdd7217145e35016519fee59751
Kandarp <patelkandarp342@gmail.com>
2019-06-18 08:49:57 -0400

    First servlet project2

---
+++ src/com/marlabs/requestinfo/ReuestHeaderInfoServlet.java
@@ -0,0 +1,60 @@
+package com.marlabs.requestinfo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ReuestHeaderInfoServlet
+ */
+public class ReuestHeaderInfoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ReuestHeaderInfoServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+        System.out.println("I am from servlet constructor");
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out= response.getWriter();
+		out.println("<html><body>");
+		out.println("<h3>Header Information Example</h3>");
+		java.util.Enumeration<String> e=request.getHeaderNames();
+		while(e.hasMoreElements()) {
+			String name= e.nextElement();
+			String value= request.getHeader(name);
+			out.println(name+" : "+value);
+		}
+		out.println("<br>");
+		out.println("========================================");
+		out.println("<h3>Header Information Example</h3>");
+		e=request.getParameterNames();
+		while(e.hasMoreElements()) {
+			String name= e.nextElement();
+			String value= request.getParameter(name);
+			out.println(name+" : "+value);
+		}
+		out.println("</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
