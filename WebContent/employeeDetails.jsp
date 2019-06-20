c582559f7e548ca6f80a0e27522d9a16a6447b6e
Kandarp <patelkandarp342@gmail.com>
2019-06-20 10:42:58 -0400

    sessin example

---
+++ WebContent/employeeDetails.jsp
@@ -0,0 +1,38 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<% 
+
+int empNum=Integer.parseInt(session.getAttribute("empNumber").toString());
+String empName=session.getAttribute("empName").toString();
+double empSalary=Integer.parseInt(session.getAttribute("empSalary").toString());
+int departmentNumber=Integer.parseInt(session.getAttribute("departmentNumber").toString());
+String empQualification=session.getAttribute("empQualification").toString();
+String empDesignation=session.getAttribute("empDesignation").toString();
+%>
+<font color="blue" size="10">
+Employee Details
+</font>
+<hr>
+
+<font color="green" size="4">
+empNumber: <%=empNum %>
+<br>
+empName: <%=empName%>
+<br>
+empSalary: <%=empSalary %>
+<br>
+DepartmentNumber: <%=departmentNumber %>
+<br>
+EmpQualification: <%=empQualification %>
+<br>
+empDesignation: <%=empDesignation %>
+</font>
+</body>
+</html>
---
+++ src/com/marlabs/sessionexample/EmployeeMoreDetails.java
@@ -0,0 +1,45 @@
+package com.marlabs.sessionexample;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class EmployeeMoreDetails
+ */
+public class EmployeeMoreDetails extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EmployeeMoreDetails() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String qualification= request.getAttribute("empQualification").toString();
+		String designation= request.getAttribute("empDesignation").toString();
+		HttpSession session=request.getSession(false);
+		session.setAttribute("empQualification", qualification);
+		session.setAttribute("empDesignation", designation);
+		response.sendRedirect("employeeDetails.jsp");
+	}
+
+}
