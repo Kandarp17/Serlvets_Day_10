a2bf882073f413055b2b18fbeff23f538725c42b
Kandarp <patelkandarp342@gmail.com>
2019-06-20 11:29:55 -0400

    sessin example

--- src/com/marlabs/sessionexample/EmployeeMoreDetails.java
+++ src/com/marlabs/sessionexample/EmployeeMoreDetails.java
@@ -34,6 +34,7 @@ public class EmployeeMoreDetails extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		System.out.println(request.getAttribute("empQualification").toString());
 		String qualification= request.getAttribute("empQualification").toString();
 		String designation= request.getAttribute("empDesignation").toString();
 		HttpSession session=request.getSession(false);
